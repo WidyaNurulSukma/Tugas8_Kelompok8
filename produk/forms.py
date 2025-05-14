e573581f2820f2123949f37f7b8091dd3a6eba0d
Pryta Rosela <132034419+prytarosela@users.noreply.github.com>
2025-05-14 12:14:38 +0700

    Update forms.py

--- produk/forms.py
+++ produk/forms.py
@@ -1,13 +1,14 @@
-from django import forms
-from .models import Produk
+from django import forms 
+from .models import Produk  
 
-class ProdukForm(forms.ModelForm):
-    class Meta:
-        model = Produk
-        fields = ['nama', 'deskripsi', 'harga', 'stok']
-        widgets = {
-            'nama': forms.TextInput(attrs={'class': 'form-control'}),
-            'deskripsi': forms.Textarea(attrs={'class': 'form-control'}),
-            'harga': forms.NumberInput(attrs={'class': 'form-control'}),
-            'stok': forms.NumberInput(attrs={'class': 'form-control'}),
+class ProdukForm(forms.ModelForm):  # Mendefinisikan kelas ProdukForm yang mewarisi dari forms.ModelForm
+    class Meta:  # Kelas Meta untuk konfigurasi form
+        model = Produk  # Menentukan model yang digunakan adalah Produk
+        fields = ['nama', 'deskripsi', 'harga', 'stok']  # Menentukan field yang akan ditampilkan di form
+
+        widgets = {  # Menentukan widget kustom untuk masing-masing field agar tampilan form lebih baik
+            'nama': forms.TextInput(attrs={'class': 'form-control'}),  # Menggunakan widget TextInput dengan class CSS 'form-control' untuk field 'nama'
+            'deskripsi': forms.Textarea(attrs={'class': 'form-control'}),  # Menggunakan widget Textarea dengan class CSS 'form-control' untuk field 'deskripsi'
+            'harga': forms.NumberInput(attrs={'class': 'form-control'}),  # Menggunakan widget NumberInput dengan class CSS 'form-control' untuk field 'harga'
+            'stok': forms.NumberInput(attrs={'class': 'form-control'}),  # Menggunakan widget NumberInput dengan class CSS 'form-control' untuk field 'stok'
         }
